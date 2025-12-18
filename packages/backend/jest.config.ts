6502d982cefe248625ba85a04795be564a2fecd2
MINU234 <kmw365849@gmail.com>
2025-12-18 17:54:08 +0900

    fix : Jest e2e 테스트 설정 수정 - jest.config.ts rootDir을 수정 (. -> ..) - pnpm-lock.yaml 업데이트

--- packages/backend/jest.config.ts
+++ packages/backend/jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json'],
-  rootDir: '.',
+  rootDir: '..',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
