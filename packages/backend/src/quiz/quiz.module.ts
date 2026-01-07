2e910da86df0d47a84d327739716c848ba88b6a1
Enble <enble_777@naver.com>
2026-01-08 03:27:47 +0900

    feat: QuizModule에 TypeORM 설정 추가

--- packages/backend/src/quiz/quiz.module.ts
+++ packages/backend/src/quiz/quiz.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { QuizGameService } from './quiz-game.service';
-import { QuizService } from './quiz.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ClovaClientService } from './clova/clova-client.service';
+import { Question } from './entity';
+import { QuizGameService } from './quiz-game.service';
 import { QuizRoundStore } from './quiz-round.store';
+import { QuizService } from './quiz.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, TypeOrmModule.forFeature([Question])],
   controllers: [],
   providers: [QuizGameService, QuizService, ClovaClientService, QuizRoundStore],
 })
